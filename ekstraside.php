5b60852ea5eb5c2815e9f5b6f3572678a316710d
Mille Wienberg <millewienberg@Milles-MacBook-Pro-2.local>
2018-12-12 17:02:27 +0100

    rettelse

--- ekstraside.php
+++ ekstraside.php
@@ -3,12 +3,32 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <link rel="stylesheet" href="CSS/ekstraside.css" type="text/css">
+
+    <link href="CSS/footer.css" rel="stylesheet">
+    <link href="CSS/header.css" rel="stylesheet">
+    <link href="CSS/main.css" rel="stylesheet">
+
+
+
+
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+
+
+
+
     <title>Event Styling</title>
 </head>
 
 <body>
+
+
+    <?php include "header.html";?>
+
     <main>
 
         <a href="eventstyling.php"><section class="styling_events" data-container>
@@ -23,6 +43,8 @@
         </section></a>
 
     </main>
+
+     <?php include "footer.html";?>
 
 </body>
 </html>
